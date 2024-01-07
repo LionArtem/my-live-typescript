b773cf8a80ea26dcaa07abe4709d69febc25e37c
Grinevich Artem <grinevitch.artemy@yandex.ru>
2024-01-07 20:01:07 +0300

    типизировать ErrServer

--- src/components/ErrServer/ErrServer.tsx
+++ src/components/ErrServer/ErrServer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Style from './ErrServer.module.scss';
 
-export default function ErrServer({ textErr }) {
+export default function ErrServer({ textErr }: { textErr: string }) {
   return (
     <div className={Style.conteiner}>
       <h2>{textErr}</h2>
