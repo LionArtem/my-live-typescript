b590be8b441f0c100c87238025a65f81cc7424ec
Grinevich Artem <grinevitch.artemy@yandex.ru>
2024-01-07 19:50:58 +0300

    типизировать ButtonsNavigationr

--- src/components/Buttons/ButtonsNavigation/ButtonsNavigation.tsx
+++ src/components/Buttons/ButtonsNavigation/ButtonsNavigation.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import Style from './ButtonsNavigation.module.scss';
 
-export default function ButtonsNavigation({ page, text }) {
+export default function ButtonsNavigation({
+  page,
+  text,
+}: {
+  page: number;
+  text: string;
+}) {
   const navigate = useNavigate();
   return (
     <p onClick={() => navigate(page)} className={Style.button}>
