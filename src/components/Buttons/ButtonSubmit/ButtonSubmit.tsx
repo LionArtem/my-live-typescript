f0fa052241ff2cee108a2578d91ff2f495921dce
Grinevich Artem <grinevitch.artemy@yandex.ru>
2024-01-07 20:00:06 +0300

    типизировать EmptyPage

--- src/components/Buttons/ButtonSubmit/ButtonSubmit.tsx
+++ src/components/Buttons/ButtonSubmit/ButtonSubmit.tsx
@@ -3,14 +3,21 @@ import Style from './ButtonSubmit.module.scss';
 import PreloaderPoint from '../../Preloaders/PreloaderPoint/PreloaderPoint';
 import TextInteractionForm from '../../TextInteractionForm/TextInteractionForm';
 
+type BottonSubmitProps = {
+  valid: boolean;
+  showPreloader: boolean;
+  successRequest: boolean;
+  textAnswerRequest: string;
+  text: string;
+};
+
 export default function BottonSubmit({
   valid,
   showPreloader,
   successRequest,
   textAnswerRequest,
   text,
-}) {
-
+}: BottonSubmitProps) {
   return (
     <div className={Style.bottonSubmit}>
       {valid ? (
--- src/components/EmptyPage/EmptyPage.tsx
+++ src/components/EmptyPage/EmptyPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Style from './EmptyPage.module.scss';
 
-export default function EmptyPage({ text }) {
+export default function EmptyPage({ text }: { text: string }) {
   return <p className={Style.emptyPage}>{text}</p>;
 }
