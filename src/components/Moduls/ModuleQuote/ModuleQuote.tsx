7d87d219341b1978efac1e5363cd6138c5b3d82a
Grinevich Artem <grinevitch.artemy@yandex.ru>
2024-01-11 20:36:22 +0300

    типизировать ModuleQuote

--- src/components/Moduls/ModuleQuote/ModuleQuote.tsx
+++ src/components/Moduls/ModuleQuote/ModuleQuote.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Stule from './ModuleQuote.module.scss';
 import ModulContainer from '../ModulContainer/ModulContainer';
 
-export default function ModuleQuote({ text, clickOverly }) {
+type ModuleQuoteProps = {
+  text: string;
+  clickOverly: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function ModuleQuote({ text, clickOverly }: ModuleQuoteProps) {
   return (
     <ModulContainer clickOverly={() => clickOverly(false)}>
       <p className={Stule.text}>{text}</p>
