97fe1366636357bec648e6a463226cadd3adec13
Grinevich Artem <grinevitch.artemy@yandex.ru>
2024-01-13 09:53:34 +0300

    типизировать MyPage

--- src/components/Pages/MyPage/MyPage.tsx
+++ src/components/Pages/MyPage/MyPage.tsx
@@ -1,35 +1,28 @@
-import React, { useState } from "react";
-import Style from "./MyPage.module.scss";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchGetUser,
-  killAllStateUser,
-  selectUser,
-} from "../../../redax/slices/userSlice";
-import { selectAuth } from "../../../redax/slices/authSlice";
-import ButtonsNavigation from "../../Buttons/ButtonsNavigation/ButtonsNavigation";
-import ErrServer from "../../ErrServer/ErrServer";
-import MyPageSceleton from "./MyPageSceleton";
-import ModuleBigFoto from "../../Moduls/ModuleBigFoto/ModuleBigFoto";
-import { URL_SERVER } from "../../../utils/Constants";
+import React, { useState } from 'react';
+import Style from './MyPage.module.scss';
+import { useSelector } from 'react-redux';
+import { fetchGetUser, selectUser } from '../../../redax/slices/userSlice';
+import { selectAuth } from '../../../redax/slices/authSlice';
+import ButtonsNavigation from '../../Buttons/ButtonsNavigation/ButtonsNavigation';
+import ErrServer from '../../ErrServer/ErrServer';
+import MyPageSceleton from './MyPageSceleton';
+import ModuleBigFoto from '../../Moduls/ModuleBigFoto/ModuleBigFoto';
+import { URL_SERVER } from '../../../utils/Constants';
+import { useAppDispatch } from '../../../redax/store';
 
 export default function MyPage() {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const { user, errServer, showSceletonPage } = useSelector(selectUser);
   const { token } = useSelector(selectAuth);
   const [showBigAvatar, isShowBigAvatar] = useState(false);
 
   React.useEffect(() => {
-    dispatch(fetchGetUser(token));
+    token && dispatch(fetchGetUser({ token }));
   }, []);
-
-  // React.useEffect(() => {
-  //   return () => dispatch(killAllStateUser());
-  // }, []);
 
   return (
     <div className={Style.use_conteiner}>
-      <ButtonsNavigation page={"/"} text={"Назад"} />
+      <ButtonsNavigation page={'/'} text={'Назад'} />
       {showSceletonPage ? (
         <MyPageSceleton />
       ) : errServer ? (
@@ -38,56 +31,58 @@ export default function MyPage() {
         <div className={Style.use_card}>
           <img
             className={
-              user.avatar
+              user?.avatar
                 ? `${Style.foto} ${Style.fotoCursor}`
                 : `${Style.foto}`
             }
             onClick={() => {
-              if (user.avatar) {
+              if (user?.avatar) {
                 isShowBigAvatar(true);
               }
             }}
             src={
-              user.avatar
+              user?.avatar
                 ? `${URL_SERVER}/${user.avatar}`
-                : "https://www.murrayglass.com/wp-content/uploads/2020/10/avatar-scaled.jpeg"
+                : 'https://www.murrayglass.com/wp-content/uploads/2020/10/avatar-scaled.jpeg'
             }
             alt="аватарка"
           />
           <ul className={Style.discription}>
             <li className={Style.title}>
               <span>Имя:</span>
-              <p>{user.name}</p>
+              <p>{user?.name}</p>
             </li>
             <li className={Style.title}>
               <span>Город:</span>
-              <p>{user.town}</p>
+              <p>{user?.town}</p>
             </li>
             <li className={Style.title}>
               <span>Возраст:</span>
-              <p>{user.age}</p>
+              <p>{user?.age}</p>
             </li>
             <li className={Style.title}>
               <span>Пол:</span>
-              <p>{user.gender}</p>
+              <p>{user?.gender}</p>
             </li>
             <li className={Style.title}>
               <span>Email:</span>
-              <p>{user.email}</p>
+              <p>{user?.email}</p>
             </li>
           </ul>
 
           <ButtonsNavigation
-            page={"/edit-user"}
-            text={"Редактировать профиль"}
+            page={'/edit-user'}
+            text={'Редактировать профиль'}
           />
         </div>
       )}
-      {showBigAvatar && (
+      {showBigAvatar && user?.avatar ? (
         <ModuleBigFoto
           isShowBigAvatar={() => isShowBigAvatar(false)}
           url={user.avatar}
         />
+      ) : (
+        ''
       )}
     </div>
   );
